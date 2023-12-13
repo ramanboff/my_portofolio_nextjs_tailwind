bea281de442c60433e5bd3f78e9fba17f5b4c3e9
ramanboff <bayramofframan@gmail.com>
2023-12-14 00:23:44 +0400

    section-3  style

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,12 @@ import raman from "../../public/dev-ed-wave.png";
 import design from "../../public/design.png";
 import code from "../../public/code.png";
 import consulting from "../../public/consulting.png";
+import web1 from "../../public/web1.png";
+import web2 from "../../public/web2.png";
+import web3 from "../../public/web3.png";
+import web4 from "../../public/web4.png";
+import web5 from "../../public/web5.png";
+import web6 from "../../public/web6.png";
 
 export default function Home() {
   return (
@@ -19,7 +25,7 @@ export default function Home() {
         <meta name="description" content="R" />
         <link rel="icon" href=".favicon.ico" />
       </Head>
-      <main className="ng-white px-10">
+      <main className="ng-white px-10 md:px-20 lg:px-40">
         {/*   section- 1 */}
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
@@ -39,11 +45,11 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
               Raman Bayramov
             </h2>
-            <h3 className="text-2xl py-2">Developer.</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">
+            <h3 className="text-2xl py-2 md-text-3xl">Developer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 max-w-xl mx-auto md:text-lg">
               Freelancer providing services for programming and design content
               needs. Join me down below and let's get cracking!
             </p>
@@ -53,7 +59,7 @@ export default function Home() {
             <AiFillLinkedin />
             <AiFillYoutube />
           </div>
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 mb-10 overflow-hidden">
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 mb-10 overflow-hidden md:h-96 md:w-96">
             <Image src={raman} alt="profile" layout="fill" objectFit="cover" />
           </div>
         </section>
@@ -61,21 +67,22 @@ export default function Home() {
         <section>
           <div>
             <h3 className="text-3xl py-1">Servives I offer</h3>
-            <p className="text-md py-2 leading-8 text-gray-800">Since the beginning of my journey as a freelance designer and
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Since the beginning of my journey as a freelance designer and
               developer, Ive done remote work for
               <span className="text-teal-500"> agencies </span>
               consulted for <span className="text-teal-500">startups </span>
               and collaborated with talanted people to create digital products
               for both business and consumer use.
-              </p>
-              <p className="text-md py-2 leading-8 text-gray-800 ">
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 ">
               I offer from a wide range of services, including brand design,
               programming and teaching.
             </p>
           </div>
-          <div className="text-center">
+          <div className="text-center lg:flex gap-10 ">
             <div className="flex flex-col items-center text-center shadow-lg p-10 rounded-xl my-10">
-              <Image src={design} width={100} height={100} alt="design"/>
+              <Image src={design} width={100} height={100} alt="design" />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
                 Beautiful Designs
               </h3>
@@ -90,7 +97,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">Indesign</p>
             </div>
             <div className="flex flex-col items-center mx-auto shadow-lg p-10 rounded-xl my-10">
-              <Image src={consulting} width={100} height={100} alt="design"/>
+              <Image src={consulting} width={100} height={100} alt="design" />
               <h3 className="text-lg font-medium pt-8 pb-2">
                 Beautiful Designs
               </h3>
@@ -105,7 +112,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">Indesign</p>
             </div>
             <div className="flex flex-col items-center shadow-lg p-10 rounded-xl my-10">
-              <Image src={code} width={100} height={100} alt="design"/>
+              <Image src={code} width={100} height={100} alt="design" />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
                 Beautiful Designs
               </h3>
@@ -118,6 +125,44 @@ export default function Home() {
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
               <p className="text-gray-800 py-1">Indesign</p>
+            </div>
+          </div>
+        </section>
+        {/*  */}
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Since the beginning of my journey as a freelance designer and
+              developer, Ive done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 ">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1">
+              <Image src={web1} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive" alt="photo"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web2} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive" alt="photo"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web3} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive" alt="photo"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web4} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive" alt="photo"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web5} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive" alt="photo"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web6} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive" alt="photo"/>
             </div>
           </div>
         </section>
