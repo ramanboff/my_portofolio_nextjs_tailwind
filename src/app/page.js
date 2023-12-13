f2d70843f88b3cff19b19203a342cb63a12efc5c
ramanboff <bayramofframan@gmail.com>
2023-12-13 21:31:52 +0400

    Name Job container and style

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import Head from "next/head";
-import {BsFillMoonStarsFill} from 'react-icons/bs';
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -14,10 +19,32 @@ export default function Home() {
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl font-burtons">Raman</h1>
             <ul className="flex items-center">
-              <li><BsFillMoonStarsFill className=" cursor-pointer text-2xl"/></li>
-              <li><a href="#" className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8">Resume</a></li>
+              <li>
+                <BsFillMoonStarsFill className=" cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                >
+                  Resume
+                </a>
+              </li>
             </ul>
           </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">Raman Bayramov</h2>
+            <h3 className="text-2xl py-2">Developer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              Freelancer providing services for programming and design content
+              needs. Join me down below and let's get cracking!
+            </p>
+          </div>
+          <div>
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillYoutube />
+          </div>
         </section>
       </main>
     </div>
